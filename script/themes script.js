b8ff5dbe547bfb415e067a125e36761f9b3018f7
Benas Janušonis <benas.janusonis@outlook.com>
2020-05-16 14:00:03 +0300

    refactor weather fetch

--- script/themes script.js
+++ script/themes script.js
@@ -1,77 +1,94 @@
 const ThemeFields = [
-    '--background-darker-shade',
-    '--background-lighter-shade',
-    '--button-text',
-    '--button-hover-text',
-    '--button-hover',
-    '--button-focus-outline',
-    '--footer-background',
-    '--footer-button-background',
-    '--weather-button-background-color',
-    '--weather-button-border-color',
-    '--weather-button-text-color'
+  '--background-darker-shade',
+  '--background-lighter-shade',
+  '--button-text',
+  '--button-hover-text',
+  '--button-hover',
+  '--button-focus-outline',
+  '--footer-background',
+  '--footer-button-background',
+  '--weather-button-background-color',
+  '--weather-button-border-color',
+  '--weather-button-text-color',
 ]
 
-function SetTheme(theme)
-{
-    window.sessionStorage.clear();
-    window.sessionStorage.setItem('theme', theme);
-    ChangeTheme();
+function SetTheme(theme) {
+  window.sessionStorage.clear()
+  window.sessionStorage.setItem('theme', theme)
+  ChangeTheme()
 }
 
-function ChangeTheme()
-{
-    let colors;
+function ChangeTheme() {
+  let colors
 
-    switch(window.sessionStorage.getItem('theme'))
-    {
-        case 'dark':
-            colors = 
-            ['161616','424242','E3F2FD','E3F2FD','161616','161616',
-            '101010','263238','2E7D32','1B5E20','000000'];
-            SetThemeToPage(colors);
-            AddInversionToArrow();
-            break;
-        case 'light':
-            colors = 
-            ['81D4FA','BDBDBD','212121','161616','ffffff','2196F3',
-            '304FFE','C5CAE9','00838F','006064', 'ffffff'];
-            SetThemeToPage(colors);
-            RemoveInversionFromArrow();
-            break;
-        default:
-            colors = 
-            ['1A237E','1E88E5','E3F2FD','263238','64B5F6','4B87B7',
-            '263238','546E7A','00695C','004D40','ffffff'];
-            SetThemeToPage(colors);
-            AddInversionToArrow();
-    }
+  switch (window.sessionStorage.getItem('theme')) {
+    case 'dark':
+      colors = [
+        '161616',
+        '424242',
+        'E3F2FD',
+        'E3F2FD',
+        '161616',
+        '161616',
+        '101010',
+        '263238',
+        '2E7D32',
+        '1B5E20',
+        '000000',
+      ]
+      SetThemeToPage(colors)
+      AddInversionToArrow()
+      break
+    case 'light':
+      colors = [
+        '81D4FA',
+        'BDBDBD',
+        '212121',
+        '161616',
+        'ffffff',
+        '2196F3',
+        '304FFE',
+        'C5CAE9',
+        '00838F',
+        '006064',
+        'ffffff',
+      ]
+      SetThemeToPage(colors)
+      RemoveInversionFromArrow()
+      break
+    default:
+      colors = [
+        '1A237E',
+        '1E88E5',
+        'E3F2FD',
+        '263238',
+        '64B5F6',
+        '4B87B7',
+        '263238',
+        '546E7A',
+        '00695C',
+        '004D40',
+        'ffffff',
+      ]
+      SetThemeToPage(colors)
+      AddInversionToArrow()
+  }
 }
 
-function SetThemeToPage(arrayOfValues)
-{
-    const root = document.documentElement.style;
-    
-    for(var i = 0; i < arrayOfValues.length; i++)
-    {
-        root.setProperty(ThemeFields[i], `#${arrayOfValues[i]}`);
-    }
+function SetThemeToPage(arrayOfValues) {
+  const root = document.documentElement.style
+
+  for (var i = 0; i < arrayOfValues.length; i++) {
+    root.setProperty(ThemeFields[i], `#${arrayOfValues[i]}`)
+  }
 }
 
-function AddInversionToArrow()
-{
-    let arrow = document.getElementById('scrollArrow');
-    if(!arrow.classList.contains('inverted'))
-    {
-        arrow.classList.add('inverted');
-    }
+function AddInversionToArrow() {
+  let arrow = document.getElementById('scrollArrow')
+  arrow.classList.add('inverted')
 }
 
-function RemoveInversionFromArrow()
-{
-    let arrow = document.getElementById('scrollArrow');
-    if(arrow.classList.contains('inverted'))
-    {
-        arrow.classList.remove('inverted');
-    }
+function RemoveInversionFromArrow() {
+  let arrow = document.getElementById('scrollArrow')
+  arrow.classList.remove('inverted')
 }
--- script/weather script.js
+++ script/weather script.js
@@ -1,87 +1,94 @@
-const apiKey = '4154b83dc3fae98323d99f1e62f85872';
+const apiKey = '4154b83dc3fae98323d99f1e62f85872'
 
-async function ShowWeatherModal(cityName)
-{
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    let request = new XMLHttpRequest();
-    request.open('GET', url);
+document
+  .querySelectorAll('.js--weatherButton')
+  .forEach((button) =>
+    button.addEventListener('click', (e) =>
+      getWeatherData(e.target.innerText).then(
+        renderWeatherModal,
+        showErrorModal
+      )
+    )
+  )
 
-    request.onload = function()
-    {
-        if(request.status >= 200 && request.status < 400)
-        {
-            OpenModal('singleCityWeatherModal');
-            let weatherData = JSON.parse(request.responseText);
-
-            console.log(weatherData);
-            renderModal(weatherData);
-        }
-    }
-
-    request.onerror = function()
-    {
-        OpenModal('errorModal');
-        let errorModal = document.getElementById("errorModal");
-        errorModal.innerText = "Check your internet connection and try again";
-    }
-
-    request.send();
+async function getWeatherData(cityName) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+  ).then(
+    (response) => response.json(),
+    () => Promise.reject('Check your internet connection and try again')
+  )
 }
 
-function renderModal(weatherData)
-{
-    let iconElement = document.getElementById("weatherIcon");
-    let descriptionElement = document.getElementById("description");
-    let temperatureElement = document.getElementById("temperature");
-    let feelsLikeTemperatureElement = document.getElementById("feelsLikeTemperature");
-    let windElement = document.getElementById("wind");
-    let locationElement = document.getElementById("location");
+function showErrorModal(message) {
+  OpenModal('errorModal')
 
-    iconElement.setAttribute("src", `../weather-icons/${weatherData.weather[0].icon}.png`);
-    descriptionElement.innerText = `${weatherData.weather[0].description}`;
-    temperatureElement.innerText = `Temperature: ${formatTemperatureData(weatherData, "temp")}`;
-    feelsLikeTemperatureElement.innerText = `Feels Like: ${formatTemperatureData(weatherData, "feels_like")}`;
-    windElement.innerText = `Wind: ${FormatWindData(weatherData)}`;
-    locationElement.innerText = `${weatherData.name}`;
+  let errorModal = document.getElementById('errorModal')
+  errorModal.innerText = message
 }
 
-function formatTemperatureData(weatherData, property)
-{
-    return `${Number(weatherData.main[property] - 273.15).toFixed(0)} °C`
+function renderWeatherModal(weatherData) {
+  OpenModal('singleCityWeatherModal')
+
+  let iconElement = document.getElementById('weatherIcon')
+  let descriptionElement = document.getElementById('description')
+  let temperatureElement = document.getElementById('temperature')
+  let feelsLikeTemperatureElement = document.getElementById(
+    'feelsLikeTemperature'
+  )
+  let windElement = document.getElementById('wind')
+  let locationElement = document.getElementById('location')
+
+  iconElement.setAttribute(
+    'src',
+    `../weather-icons/${weatherData.weather[0].icon}.png`
+  )
+  descriptionElement.innerText = `${weatherData.weather[0].description}`
+  temperatureElement.innerText = `Temperature: ${formatTemperatureData(
+    weatherData,
+    'temp'
+  )}`
+  feelsLikeTemperatureElement.innerText = `Feels Like: ${formatTemperatureData(
+    weatherData,
+    'feels_like'
+  )}`
+  windElement.innerText = `Wind: ${FormatWindData(weatherData)}`
+  locationElement.innerText = `${weatherData.name}`
 }
 
-function FormatWindData(weatherData)
-{
-    let degrees = weatherData.wind.deg;
-    let windSpeed = Number(weatherData.wind.speed).toFixed(1);
-
-    if(degrees > 337.5 || degrees <= 22.5) return `← ${windSpeed} m/s`;
-    if(degrees <= 67.5) return `↙ ${windSpeed} m/s`;
-    if(degrees <= 112.5) return `↓ ${windSpeed} m/s`;
-    if(degrees <= 157.5) return `↘ ${windSpeed} m/s`;
-    if(degrees <= 202.5) return `→ ${windSpeed} m/s`;
-    if(degrees <= 247.5) return `↗ ${windSpeed} m/s`;
-    if(degrees <= 292.5) return `↑ ${windSpeed} m/s`;
-    if(degrees <= 337.5) return `↖ ${windSpeed} m/s`;
-    else return `${windSpeed} m/s`; //in case wind direction is not received from the API
+function formatTemperatureData(weatherData, property) {
+  return `${Number(weatherData.main[property] - 273.15).toFixed(0)} °C`
 }
 
-function OpenModal(modalID)
-{
-    let overlay = document.getElementById('overlay');
-    let modal = document.getElementById(modalID);
+function FormatWindData(weatherData) {
+  let degrees = weatherData.wind.deg
+  let windSpeed = Number(weatherData.wind.speed).toFixed(1)
 
-    modal.classList.add('active');
-    overlay.classList.add('active');
+  if (degrees > 337.5 || degrees <= 22.5) return `← ${windSpeed} m/s`
+  if (degrees <= 67.5) return `↙ ${windSpeed} m/s`
+  if (degrees <= 112.5) return `↓ ${windSpeed} m/s`
+  if (degrees <= 157.5) return `↘ ${windSpeed} m/s`
+  if (degrees <= 202.5) return `→ ${windSpeed} m/s`
+  if (degrees <= 247.5) return `↗ ${windSpeed} m/s`
+  if (degrees <= 292.5) return `↑ ${windSpeed} m/s`
+  if (degrees <= 337.5) return `↖ ${windSpeed} m/s`
+  else return `${windSpeed} m/s` //in case wind direction is not received from the API
 }
 
-function CloseModal()
-{
-    let overlay = document.getElementById('overlay');
-    let weatherModal = document.getElementById('singleCityWeatherModal');
-    let errorModal = document.getElementById('errorModal');
+function OpenModal(modalID) {
+  let overlay = document.getElementById('overlay')
+  let modal = document.getElementById(modalID)
 
-    overlay.classList.remove('active');
-    weatherModal.classList.remove('active');
-    errorModal.classList.remove('active');
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function CloseModal() {
+  let overlay = document.getElementById('overlay')
+  let weatherModal = document.getElementById('singleCityWeatherModal')
+  let errorModal = document.getElementById('errorModal')
+
+  overlay.classList.remove('active')
+  weatherModal.classList.remove('active')
+  errorModal.classList.remove('active')
 }
